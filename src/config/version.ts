625639dd234cb33862106fbc801f28669a0d5a3c
Patrick Eymard <patrick.eymard@actifit.pro>
2025-12-10 13:55:17 +0100

    Nouvelle indentation et préparation pour nouvelles modifications (v8.14.9)

--- src/config/version.ts
+++ src/config/version.ts
@@ -4,6 +4,8 @@ import packageJson from '../../package.json';
 export const APP_VERSION = packageJson.version;
 export const VERSION_DISPLAY = `v${packageJson.version}`;
 
+// Préparation pour nouvelles modifications
+
 
 
 
