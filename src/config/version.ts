0d89d6fe474fd8ce8c403e482aebdf7ee8706bdf
Patrick Eymard <patrick.eymard@actifit.pro>
2025-12-11 09:52:11 +0100

    Initialisation session - nouvelle indentation second niveau (v8.16.0) (v8.16.1)

--- src/config/version.ts
+++ src/config/version.ts
@@ -4,7 +4,7 @@ import packageJson from '../../package.json';
 export const APP_VERSION = packageJson.version;
 export const VERSION_DISPLAY = `v${packageJson.version}`;
 
-// Préparation pour nouvelles modifications - Session initialisée
+// Préparation pour nouvelles modifications - Session initialisée (v8.16.0)
 
 
 
