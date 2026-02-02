d399b8c3ccf0813378bdea9855357b12bb723410
Patrick Eymard <peymard@somone.fr>
2026-02-02 14:45:32 +0100

    Revert: Retour architecture monolithique api/index.ts (multi-fichiers incompatible Vercel) (v17.11.1)

--- api/_lib/index.ts
+++
@@ -1,8 +0,0 @@
-// SOMONE Cockpit Studio - Exports centralisés
-// Ce dossier _lib/ est ignoré par Vercel comme route API
-// mais ses fichiers sont importables par les autres routes
-
-export * from './types';
-export * from './config';
-export * from './db';
-export * from './auth';
